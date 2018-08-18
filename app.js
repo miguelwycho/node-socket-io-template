c39fe62ad871bae925b71124a8952ea9c7202362
Miguel Wychovaniec <miguelwychoi@gmail.com>
2018-08-18 13:59:29 -0300

    First Commit: Basic Node-Socket-io Template.

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var PORT = 3000;
+/**
+  Creates and initializes Express server
+*/
+var server = app.listen(PORT);
+/**
+  Declare a new web socket listening on Express server
+*/
+var io = require('socket.io').listen(server);
+/**
+  This part of the code listen to events coming from the socket
+  and respond to then based on channel
+*/
+
+io.on('connection',function(socket){
+  console.log('Client connected');
+  socket.on('message',function(message){
+    console.log('Socket connection works!');
+  })
+  socket.on('disconnect',function(message){
+    console.log('Client disconnected');
+  })
+
+})
