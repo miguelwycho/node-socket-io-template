02a21de0aa739e008c7b1b4bb1e333d34e2966e2
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-18 22:17:39 -0300

    Admin Page

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
-var app = require('express')();
-var PORT = 3000;
+var express = require("express"), http = require('http');
+var app = express();
+var server = http.createServer(app);
+var port  = 5000;
+var io = require('socket.io')(server);
+server.listen(port, '0.0.0.0');
+
+
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'secret',
-  database: 'zorbao'
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
 });
 
-/**
-  Creates and initializes Express server
-*/
-var server = app.listen(PORT, () => {
-  console.log("Listening on port", PORT);
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+ 
 });
-/**
-  Declare a new web socket listening on Express server
-*/
-var io = require('socket.io').listen(server);
-/**
-  This part of the code listen to events coming from the socket
-  and respond to then based on channel
-*/
 
 io.on('connection', function (socket) {
   console.log('Client connected');
   socket.on('message', function (message) {
-    console.log('Socket connection works!');
+    console.log('Message:', message);
+    let params = [message[0], message[1], message[2]];
+    con.query("INSERT INTO zorbao.contact_messages SET name=?,email=?,message=?; ", params, function (error,res) {
+      if (error) throw error;
+    });
+
   })
-  socket.on('disconnect', function (message) {
+  socket.on('disconnect', function () {
     console.log('Client disconnected');
   })
 
-})
-//Initialize connection to mysql
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
+  socket.on('fetch_user' , function(){
+    con.query("SELECT * FROM zorbao.team_info ", function (error,res) {
+      if (error) throw error;
+      console.log('users get')
+      io.emit('user',res);
+    });
+  })
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-});
-//Closes connection to mysql
-connection.end();
+  socket.on('update' , function(message){
+    let params = [];
+    params.push(message.name);
+    params.push(message.city);
+    params.push(message.age);
+    params.push(message.position);
+    params.push(message.idteam_info);
+    con.query("update zorbao.team_info set name =?,city =?,age=?,position=? where idteam_info =?;",params, function (error,res) {
+      if (error) throw error;
+    });
+  })
+  
+
+})
